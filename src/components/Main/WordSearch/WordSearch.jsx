f5754a6c9d3bbd392f951f93f5c0a47ab049bc55
King Macking <matiasatzori@gmail.com>
2023-03-24 10:48:28 -0300

    FIX: Autofocus on search bar, on focus remove outline

--- src/components/Main/WordSearch/WordSearch.jsx
+++ src/components/Main/WordSearch/WordSearch.jsx
@@ -17,8 +17,8 @@ const WordSearch = ({setWordToSearch}) => {
     return (
         <div className="w-full mt-10">
             <form className="flex items-center w-full" onSubmit={handleSubmit}>
-                <input type="text" name="word" className="w-full px-3 py-2 text-3xl border border-r-0 dark:bg-blackDarker border-grey dark:text-white rounded-l-md" placeholder="Enter word to search here"/>
-                <button type="submit" className="px-3 py-2 text-4xl border border-l-0 dark:bg-blackDarker border-grey rounded-r-md text-blackLighter dark:text-white"><Icon icon="material-symbols:manage-search-rounded" inline={true} /></button>
+                <input autoFocus={true} type="text" name="word" className="w-full outline-none focus:outline-none px-3 py-4 text-3xl border border-r-0 dark:bg-blackDarker border-grey dark:text-white rounded-l-md" placeholder="Enter word to search here"/>
+                <button type="submit" className="px-3 py-4 text-4xl border border-l-0 dark:bg-blackDarker border-grey rounded-r-md text-blackLighter dark:text-white"><Icon icon="material-symbols:manage-search-rounded" inline={true} /></button>
             </form>
             {invalidSearch && <p className="pl-2 text-sm text-vermilion">You should enter a valid word to find!</p>}
         </div>
